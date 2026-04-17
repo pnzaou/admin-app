ba734eab71b6466e5647cc918fbad5276130272b
Perrin Emmanuel Nzaou <perrinemmanuelnzaou@gmail.com>
2026-04-17 17:44:46 +0000

    Création du contenu de l'entité AppUser

--- src/main/java/sn/isi/entities/AppUser.java
+++ src/main/java/sn/isi/entities/AppUser.java
@@ -1,4 +1,35 @@
 package sn.isi.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 150)
+    private String nom;
+
+    @Column(nullable = false, length = 200)
+    private String prenom;
+
+    @Column(unique = true, nullable = false, length = 150)
+    private String email;
+
+    @Column(nullable = false, length = 150)
+    private String password;
+
+    private int etat;
+
+    @ManyToMany()
+    private List<AppRoles> appRoles;
+    @OneToMany(mappedBy = "appUser")
+    private List<Produit> produits;
 }
--- src/main/java/sn/isi/entities/Produit.java
+++ src/main/java/sn/isi/entities/Produit.java
@@ -1,4 +1,13 @@
 package sn.isi.entities;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Produit {
 }
