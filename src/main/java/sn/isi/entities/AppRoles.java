d630b4788977b244b72d020597d5990ebabc4ce6
Perrin Emmanuel Nzaou <perrinemmanuelnzaou@gmail.com>
2026-04-17 17:37:32 +0000

    Création du contenu de l'entité AppRoles

--- src/main/java/sn/isi/entities/AppRoles.java
+++ src/main/java/sn/isi/entities/AppRoles.java
@@ -1,4 +1,20 @@
 package sn.isi.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AppRoles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(unique = true, nullable = false, length = 100)
+    private String nom;
+    @ManyToMany(mappedBy = "appRoles")
+    private List<AppUser> appUser;
 }
