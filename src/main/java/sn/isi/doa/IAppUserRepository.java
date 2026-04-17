956a127020bec0d97bd1ae7d74e6dc559b1efcf4
Perrin Emmanuel Nzaou <perrinemmanuelnzaou@gmail.com>
2026-04-17 17:54:06 +0000

    Création d'une méthode qui récupère un utilisateur à partir de son adresse mail

--- src/main/java/sn/isi/doa/IAppUserRepository.java
+++ src/main/java/sn/isi/doa/IAppUserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import sn.isi.entities.AppUser;
 
 public interface IAppUserRepository extends JpaRepository<AppUser, Integer> {
+    AppUser findByEmail(String email);
 }
--- src/main/java/sn/isi/doa/IProduitRepository.java
+++ src/main/java/sn/isi/doa/IProduitRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import sn.isi.entities.Produit;
 
 public interface IProduitRepository extends JpaRepository<Produit, Integer> {
+
 }
